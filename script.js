3e050c34bbc816cd8ebc26eda6e4602b06a83eb1
nicholasreedingersoll-debug <nicholasreedingersoll@gmail.com>
2026-05-11 18:37:49 +0000

    nmhj

--- script.js
+++ script.js
@@ -2,14 +2,23 @@ document.getElementById("reset-btn").addEventListener("click", resetGame);
 document.getElementById("rock-btn").addEventListener("click", chosenItem);
 document.getElementById("paper-btn").addEventListener("click", chosenItem);
 document.getElementById("scissors-btn").addEventListener("click", chosenItem);
-document.getElementById("bst1").addEventListener("click");
-document.getElementById("bst3").addEventListener("click");
-document.getElementById("bst5").addEventListener("click");
+document.getElementById("bst1").addEventListener("click", setBestOf);
+document.getElementById("bst3").addEventListener("click", setBestOf);
+document.getElementById("bst5").addEventListener("click", setBestOf);
 
 let choices = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
-let targetScore = 3;
+let targetScore = 1;
+
+function setBestOf(event) {
+  let buttonId = event.target.id;
+  if (buttonId == "bst1") targetScore = 1;
+  if (buttonId == "bst3") targetScore = 2;
+  if (buttonId == "bst5") targetScore = 3;
+  resetGame();
+  document.getElementById("round-result").innerText = "Best of " + (targetScore * 2 - 1) + " | Make your choice!";
+}
 
 function chosenItem(event) {
   let computerChoice = choices[Math.floor(Math.random() * 3)];
@@ -31,10 +40,17 @@ function getWinner(player, computer) {
   return "lose";
 }
 
-function updateDisplay(playerChoice, computerChoice) {
+function updateDisplay(playerChoice, computerChoice, result) {
   document.getElementById("player-choice").innerText = "You chose: " + playerChoice;
   document.getElementById("computer-choice").innerText = "Computer chose: " + computerChoice;
   
+  if (result == "win") {
+    document.getElementById("round-result").innerText = "You won this round!";
+  } else if (result == "lose") {
+    document.getElementById("round-result").innerText = "Computer won this round!";
+  } else {
+    document.getElementById("round-result").innerText = "It's a tie!";
+  }
 }
 
 function updateScore(result) {
@@ -58,9 +74,17 @@ function resetGame() {
 
 function bestOf() {
   if (playerScore == targetScore) {
-    document.getElementById("round-result").innerText = "You won the game!";
+    document.getElementById("round-result").innerText = "You won the Best of " + (targetScore * 2 - 1) + "! Starting new game...";
+    setTimeout(function() {
+      resetGame();
+      document.getElementById("round-result").innerText = "Best of " + (targetScore * 2 - 1) + " | Make your choice!";
+    }, 2000);
   }
   if (computerScore == targetScore) {
-    document.getElementById("round-result").innerText = "Computer won the game!";
+    document.getElementById("round-result").innerText = "Computer won the Best of " + (targetScore * 2 - 1) + "! Starting new game...";
+    setTimeout(function() {
+      resetGame();
+      document.getElementById("round-result").innerText = "Best of " + (targetScore * 2 - 1) + " | Make your choice!";
+    }, 2000);
   }
 }
